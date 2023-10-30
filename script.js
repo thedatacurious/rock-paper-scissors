1039a918a1e33ce3aca08b32a2ee806fb810edac
Alexandra Khoo <43926254+thedatacurious@users.noreply.github.com>
2023-10-31 00:44:58 +0800

    Create UI for gameplay

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+const container = document.querySelector("#container");
+const resultDisplay = document.createElement("div");
+
+const rock = document.createElement("button");
+rock.textContent = "Rock";
+rock.setAttribute("id", "rock");
+
+const scissors = document.createElement("button");
+scissors.textContent = "Scissors";
+scissors.setAttribute("id", "scissors");
+
+const paper = document.createElement("button");
+paper.textContent = "Paper";
+paper.setAttribute("id", "paper");
+
+container.appendChild(resultDisplay);
+container.appendChild(rock);
+container.appendChild(scissors);
+container.appendChild(paper);
+
+const record = {
+  winRate: 0,
+  count: 1,
+  result: "",
+};
+
+container.addEventListener("click", (e) => {
+  let target = e.target;
+  let playerSelection;
+  let computerSelection = getComputerChoice();
+  switch (target.id) {
+    case "rock":
+      playerSelection = "Rock";
+      break;
+    case "scissors":
+      playerSelection = "Scissors";
+      break;
+    case "paper":
+      playerSelection = "Paper";
+      break;
+  }
+
+  record.result = playRound(playerSelection, computerSelection);
+  record.count += 1;
+  resultDisplay.textContent = `${record.result} Pick again for round ${record.count} out of 5.`;
+
+  if (record.result.includes("win")) {
+    record.winRate += 1;
+  }
+
+  if (record.count === 6) {
+    resultDisplay.textContent = `${record.result} 
+    Game over. You've won ${record.winRate}x.`;
+    container.removeChild(rock);
+    container.removeChild(scissors);
+    container.removeChild(paper);
+  }
+});
+
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   let index = Math.floor(Math.random() * 3);
@@ -5,36 +64,23 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
   if (
     (playerSelection === "Rock" && computerSelection === "Scissors") ||
     (playerSelection === "Scissors" && computerSelection === "Paper") ||
     (playerSelection === "Paper" && computerSelection === "Rock")
   ) {
     return `You win! ${playerSelection} beats ${computerSelection}!`;
-  } else if (playerSelection === computerSelection){
-    return "It's a tie, please try again!"
-  }
-  else {
+  } else if (playerSelection === computerSelection) {
+    return `It's a tie! The computer chose ${playerSelection} too.`;
+  } else {
     return `You lose! ${computerSelection} beats ${playerSelection}!`;
   }
 }
 
 
-function game(){
-    let winRate  = 0;
-    for(count = 1; count < 6; count ++){
-        console.log("We're playing round", count, "out of 5");
-        let playerSelection = prompt("Pick your weapon (Rock, Scissors, or Paper)")
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection,computerSelection);
-        if (result.includes("win")){
-            winRate += 1;
-        }
-        console.log(result);
-    }
-    console.log("You've won", winRate, "x")
 
-}
+  resultDisplay.textContent = `Pick an option.This is round ${record.count} out of 5.`;
 
-game();
+
+
+
